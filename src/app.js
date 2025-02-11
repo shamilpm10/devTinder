90577a7f95e09be2f84c8da5be40c1401f61399c
shamilpm10 <shamilpmattil@gmail.com>
2025-02-11 15:44:28 +0530

    exploring GET,POST,DELETE methods

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
 const express=require("express")
 const app=express();
 
-app.use("/demo",(req,res)=>{
-    res.send("welcome to the server")
+
+
+app.get("/abc",(req,res)=>{
+    res.send({firstname:"maguire",lastname:"goat"})
 })
 
-app.listen(7777,()=>{
+// app.get("/user/abc",(req,res)=>{
+//     res.send("userabc")
+// })
+
+app.delete("/user",(req,res)=>{
+    res.send("data deleted")
+})
+// app.use("/user",(req,res)=>{
+//     res.send("hellooo")
+// })
+
+app.post("/user",(req,res)=>{
+    //data saved to database
+    res.send("data saved successfully to database")
+})
+
+
+
+
+
+
+
+
+app.listen(3000,()=>{
     console.log("server iistening on port 7777");
     
 })
