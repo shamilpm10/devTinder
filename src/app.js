b61466d2dddde8980102fa6d24d37ce58fa3c53c
shamilpm10 <shamilpmattil@gmail.com>
2025-02-19 23:35:34 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -20,6 +20,39 @@ app.post("/signup",async(req,res)=>{
 
 })
 
+app.get("/feed",async(req,res)=>{
+    try{
+        const users= await User.find({})
+        
+        if(!users.length){
+            res.status(404).send("data cannot be found")
+            
+        }
+        else{
+            res.send(users)
+        }
+    }catch(err){
+        console.log("something went wrong");
+        
+    }
+})
+
+app.get("/user",async(req,res)=>{
+    try{
+        const users=await User.find({age:req.body.age})
+        if(users.length){
+            res.send(users)
+        }
+        else{
+            res.status(404).send("cannot find the data")
+        }
+
+    } catch(err){
+        res.status(400).send("something went wrong"+err.messsge)
+    }
+
+})
+
 connectDB().then(()=>{
     console.log("database connection established");
     app.listen(3000,()=>{
